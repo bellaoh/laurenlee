7a2cf1eb05a633d683254d3c64d55003c16925d2
Eunjeong <oheunjeo@msu.edu>
2020-05-09 04:34:50 -0500

    Updates

---
+++ precache-manifest.b881b20e3a0c6b60d2944f65cbb68cc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffde0699bd522b6f7bfc293f1538968e",
+    "url": "/lauren/index.html"
+  },
+  {
+    "revision": "ea950eac7cf320ebb10a",
+    "url": "/lauren/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "225001f9f67426d71b60",
+    "url": "/lauren/static/js/2.616ae0b6.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/lauren/static/js/2.616ae0b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea950eac7cf320ebb10a",
+    "url": "/lauren/static/js/main.205c195f.chunk.js"
+  },
+  {
+    "revision": "0c7627bba0fb568dfafc",
+    "url": "/lauren/static/js/runtime-main.92e87278.js"
+  }
+]);
